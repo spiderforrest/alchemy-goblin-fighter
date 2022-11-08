9fbca039215998f18b0fa68628bee61c75c9024e
spiderforrest <spider@spood.org>
2022-11-08 12:09:36 -0800

    spawn two goblins on load commit-added index, functions to use an array to make dom elements

--- app.js
+++ app.js
@@ -1,11 +1,56 @@
-/* Imports */
+const wincount = document.getElementById('wincount');
+const health = document.getElementById('health');
+const player = document.getElementById('player');
+const addGoblinInput = document.getElementById('add-goblin-input');
+const submit = document.getElementById('submit');
+const goblins = document.getElementById('goblins');
 
-/* Get DOM Elements */
+let hp = 10;
+let wins = 0;
+let goblinArray = [];
 
-/* State */
+// adds a new goblin
+function makeGoblin(name, health) {
+    const newGoblin = {
+        name: name,
+        hp: health,
+    };
+    goblinArray.push(newGoblin);
+}
 
-/* Events */
+// put together the goblin div
+function assembleGoblin(goblin, index) {
+    const goblinDiv = document.createElement('div');
+    const nameH = document.createElement('h3');
+    const hpP = document.createElement('p');
 
-/* Display Functions */
+    nameH.textContent = goblin.name;
+    hpP.id = `goblin-health-${index}`;
+    hpP.textContent = `health: ${goblin.hp}`;
+    goblinDiv.append(nameH, hpP);
 
-// (don't forget to call any display functions you want to run on page load!)
+    return goblinDiv;
+}
+
+// todo: write the actual game code here
+function attackHandler() {}
+
+// iterate over array, showing goblins, you know the drill
+function displayGoblins() {
+    goblins.textContent = '';
+    for (const [index, item] of goblinArray.entries()) {
+        const target = assembleGoblin(item, index);
+        target.addEventListener('click', () => {
+            attackHandler(item);
+        });
+        goblins.append(target);
+    }
+}
+
+function init() {
+    makeGoblin('Ackazar, The Mighty, Ruler of Mordor', 1);
+    makeGoblin('bob', 5);
+    displayGoblins();
+}
+init();
+console.log(goblinArray);
